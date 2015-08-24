c8e9249049ef616db85649d9713a821bdf8de46c
Pablo de la Pena <p@shedcollective.org>
2015-08-24 23:39:19 +0100

    Finally...

--- icsmerger.rb
+++ icsmerger.rb
@@ -6,7 +6,12 @@ class Icsmerger < Formula
   sha1 "9b1db327d1f865bde52add80b338dd1bcd000d39"
 
   def install
-    prefix.install "icsmerger.phar"
-    bin.install_symlink prefix/"icsmerger.phar"
+    libexec.install "icsmerger.phar"
+    (libexec/"icsmerger").write <<-EOS.undent
+      #!/usr/bin/env bash
+      /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/icsmerger.phar $*
+      EOS
+    chmod 0755, (libexec/"icsmerger")
+    bin.install_symlink (libexec/"icsmerger")
   end
 end
