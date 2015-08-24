a81029e0fbed6f3982d3659db5e82ca0a3a7801c
Pablo de la Pena <p@shedcollective.org>
2015-08-24 23:03:53 +0100

    Initial commit
    Including recipe for `icsmerger` 1.0.0.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Homebrew Recipes
+
+# Installation
+
+Tap this repository in order to downlaod these formula via brew
+
+    brew tap hellopablo/utilities
+
+## Available Recipes
+
+### `icsmerger`
+
+A simple tool for merging multiple .ics files into one, see [the tool's main repository](https://github.com/hellopablo/ics-merger) for more information.
---
+++ icsmerger.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class Icsmerger < Formula
+  homepage "https://github.com/hellopablo/icsmerger"
+  url "https://github.com/hellopablo/ics-merger/releases/download/1.0.0/icsmerger.phar"
+  sha1 "9b1db327d1f865bde52add80b338dd1bcd000d39"
+
+  def install
+    prefix.install "icsmerger"
+    bin.install_symlink prefix/"icsmerger"
+  end
+end
