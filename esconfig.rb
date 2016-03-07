dfeddee6917bc36956225cf799466b50dce00378
Pablo de la Peña <p@shedcollective.org>
2016-03-07 14:02:17 +0000

    Bumped esconfig to v2.0.0

--- esconfig.rb
+++ esconfig.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Esconfig < Formula
   homepage "https://github.com/hellopablo/esconfig"
-  url "https://github.com/hellopablo/esconfig/releases/download/1.0.0/esconfig.phar"
-  sha1 "29f9a961ecf361f437bd3a7ac5d1596382ac7917"
+  url "https://github.com/hellopablo/esconfig/releases/download/2.0.0/esconfig.phar"
+  sha1 "d8a4e28a1b7c723db0cb513be17fcf131659e3d6"
 
   def install
     libexec.install "esconfig.phar"
